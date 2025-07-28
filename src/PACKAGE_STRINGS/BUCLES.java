3fcd0de2f7a5255e70f209032d88a445caca9ab4
GONZALO VAZQUEZ ZUBIMENDI (M14121985) <matiasvazquez141285@gmail.com>
2025-07-28 23:46:09 +0200

    ultimos cambios 28/07/25 23:46

--- src/PACKAGE_STRINGS/BUCLES.java
+++ src/PACKAGE_STRINGS/BUCLES.java
@@ -46,12 +46,12 @@ public void buclesFor(){
 public void buclesForEach(){
         //tabla de multiplicar con bucle for each
         System.out.println("Tablas de multiplicar del 0 al 10 CON BUCLE FOR EACH");
-        int [] envoltorio= {0,1,2,3,4,5,6,7,8,9,10};
-        int[] tablaMultiplicar = {0,1,2,3,4,5,6,7,8,9,10};
+        int [] multiplicando= {0,1,2,3,4,5,6,7,8,9,10};
+        int[] multiplicador = {0,1,2,3,4,5,6,7,8,9,10};
 
-        for(int elemento:envoltorio){
+        for(int elemento : multiplicando){
             System.out.println("Tabla de multiplicar del "+elemento);
-            for(int n :tablaMultiplicar){
+            for(int n : multiplicador){
                 System.out.println(String.format(" %d x %d = %d ",elemento,n, elemento*n));
 
             }
